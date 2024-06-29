7a80bc0f879c4504690ceb5025c552f2a50e8ac5
AntonyLaurente2002 <95670297+AntonyLaurente2002@users.noreply.github.com>
2024-06-29 13:22:00 -0500

    Add files via upload

---
+++ Migrations/20240629180826_v1_crear_base_de_datos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab13C.Migrations
+{
+    public partial class v1_crear_base_de_datos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
